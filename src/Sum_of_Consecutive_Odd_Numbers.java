abdb13917688f68fd78e58c570a5f769fba09c32
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-11-16 20:58:29 +0200

    Add files via upload

--- src/Sum_of_Consecutive_Odd_Numbers.java
+++ src/Sum_of_Consecutive_Odd_Numbers.java
@@ -1,23 +1,25 @@
 import java.util.Scanner;
 
 public class Sum_of_Consecutive_Odd_Numbers {
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
-        int sum = 0;
-        int l = in.nextInt();
-        for (int x = 0; x < l; x++) {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int l= in.nextInt();
+        int sum=0;
+        for (int i=0;i<l;l++){
             sum=0;
-            int n = in.nextInt();
-            int m = in.nextInt();
-            int max=Math.max(n,m); //عشان لو عكس الارقام
-            int min=Math.min(n,m);
-            for (int i = min+1; i < max; i++) {
-                if (i % 2 != 0) {
-                    sum += i;
-                }
+            int num1= in.nextInt();
 
+            int num2= in.nextInt();
+            int max=Math.max(num1 , num2);
+            int min=Math.min(num1 , num2);
+            for(int j=min+1 ;j<max;j++){
+               if (j%2 !=0){
+                   sum+=j;
+               }
             }
             System.out.println(sum);
         }
+
     }
 }
+
