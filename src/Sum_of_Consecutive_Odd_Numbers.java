05e385fa0d78c6203662e29f03d10f81781c8a23
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-11-16 20:50:26 +0200

    codeforce by java

--- src/Sum_of_Consecutive_Odd_Numbers.java
+++ src/Sum_of_Consecutive_Odd_Numbers.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Sum_of_Consecutive_Odd_Numbers {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in=new Scanner(System.in);
+        int l= in.nextInt();
+        int sum=0;
+        for (int i=0;i<l;l++){
+            sum=0;
+            int num1= in.nextInt();
+
+            int num2= in.nextInt();
+            int max=Math.max(num1 , num2);
+            int min=Math.min(num1 , num2);
+            for(int j=min+1 ;j<max;j++){
+               if (j%2 !=0){
+                   sum+=j;
+               }
+            }
+            System.out.println(sum);
+        }
+
     }
 }
+
