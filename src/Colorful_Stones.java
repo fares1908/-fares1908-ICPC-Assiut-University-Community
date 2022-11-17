abcca0617e3b2043288df6b26776b567991de613
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-11-17 07:58:27 +0200

    Update Colorful_Stones.java
    codeforce

--- src/Colorful_Stones.java
+++ src/Colorful_Stones.java
@@ -6,10 +6,10 @@ public class Colorful_Stones {
         Scanner in=new Scanner(System.in);
         String x= in.next();
         String y=in.next();
-        int counter=1;
+        int counter=1;//الكونتر و الزد بيشاوروا علي الاكس
         int z=0;
         for (int i=0;i<y.length();i++){
-            if(x.toCharArray()[z]==y.toCharArray()[i]){
+            if(x.toCharArray()[z]==y.toCharArray()[i]){ //هنا استخدمت توشار ارري عشان اعرض الاسرينج ع شكل صفوف جمب بعض
                 counter++;
                 z++;
             }
