55e5102de7f5e5908338a5e3258fd2b8cc6c45ab
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-12-06 10:01:53 +0200

    Add files via upload

---
+++ sheet2/A_1_to_N.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class A_1_to_N {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x= in.nextInt();
+        for (int i=1 ; i<=x;i++)
+            System.out.println(i);
+    }
+}
