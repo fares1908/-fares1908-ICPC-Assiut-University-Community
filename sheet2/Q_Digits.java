74d43f635fb8e1b717ca52c2fcf7626cafc5470e
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-12-13 08:27:22 +0200

    Update Q_Digits.java

--- sheet2/Q_Digits.java
+++ sheet2/Q_Digits.java
@@ -19,3 +19,26 @@ public class Q_Digits {
         }
     }
 }
+//حل اخر
+/*
+import java.util.Scanner;
+
+public class Q_Digits {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+       while (n>0){
+            String x = in.next();
+
+            for (int j = x.length() - 1; j >= 0; j--) {
+                System.out.print(x.charAt(j) + " ");
+            }
+            System.out.println();
+            n--;
+
+
+        }
+    }
+}
+*/
