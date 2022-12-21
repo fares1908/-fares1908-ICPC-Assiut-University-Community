53bbafe93984c2f2d6172ad5e4f9e4255c323c23
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2022-12-21 20:46:01 +0200

    Add files via upload

--- sheet2/T_Shape2.java
+++ sheet2/T_Shape2.java
@@ -5,13 +5,20 @@ public class T_Shape2 {
         Scanner in = new Scanner(System.in);
         int x= in.nextInt();
 
-        for (int i=1;i<=x;i++){
-            for (int s=0;s<x-i;s++)
+        for (int i=1;i<=x;i++){//عدد الخطوط
+            for (int s=0;s<x-i;s++)//المساحات الفاضيه
                 System.out.print(" ");
                for (int k=1;k<=(i*2)-1;k++)
                    System.out.print("*");
             System.out.println();
             }
+        for (int i=x;i>=1;i--){
+            for (int s=x-i;s>0;s--)
+                System.out.print(" ");
+            for (int k=1;k<=(i*2)-1;k++)
+                System.out.print("*");
+            System.out.println();
+        }
 
 
     }
