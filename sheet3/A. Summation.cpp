361f015712f82b778839262ce2f4f9f3e1f5be6b
FARES SAMY <109601304+fares1908@users.noreply.github.com>
2023-02-12 15:48:29 +0200

    Add files via upload

---
+++ sheet3/A. Summation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int x;
+	cin>>x;
+	long long sum =0;
+for(int i=0;i<x;i++){
+	int num =0;
+	cin>>num;
+sum=sum+num;
+}
+cout<<abs(sum);
+}
+
